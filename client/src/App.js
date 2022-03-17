0f3773dd9249119fbe9c35846c5c1ef1f28847c4
Ed Walker <ed.walker@optix.co.uk>
2022-03-17 10:15:14 +0000

    now with client

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Apollo App</h1>
+      <ExchangeRates></ExchangeRates>
+    </div>
+  );
+}
+
+export default App;
